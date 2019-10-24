e2d79268a00ebee200a6ee5d2ec96c89743041c0
Adedeji <aadedeji1@MC111553.local>
2019-10-24 15:28:07 +0100

    added network connectivtiy check

--- api/src/main/java/com/ayo/api/di/NetworkModule.kt
+++ api/src/main/java/com/ayo/api/di/NetworkModule.kt
@@ -1,6 +1,8 @@
 package com.ayo.api.di
 
+import android.content.Context
 import com.ayo.api.endpoints.MovieDbEndpoints
+import com.ayo.api.interceptors.NetworkConnectivityInterceptor
 import com.ayo.api.interceptors.NetworkResponseInterceptor
 import com.ayo.api.utils.addQueryParameterInterceptor
 import dagger.Module
@@ -8,11 +10,13 @@ import dagger.Provides
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 @Module
 class NetworkModule {
 
     companion object {
+        private const val TIME_OUT = 1L
         private const val BASE_URL = "https://api.themoviedb.org/"
         private const val API_KEY_FIELD = "api_key"
         private const val API_KEY = "d3b018581c65b4ac18d55a61391e87ac"
@@ -33,10 +37,13 @@ class NetworkModule {
     }
 
     @Provides
-    fun provideMovieDbHttpClient(): OkHttpClient {
+    fun provideMovieDbHttpClient(context: Context): OkHttpClient {
         return OkHttpClient.Builder()
             .addInterceptor(NetworkResponseInterceptor())
+            .addInterceptor(NetworkConnectivityInterceptor(context))
             .addQueryParameterInterceptor(API_KEY_FIELD, API_KEY)
+            .readTimeout(TIME_OUT, TimeUnit.MINUTES)
+            .connectTimeout(TIME_OUT, TimeUnit.MINUTES)
             .build()
     }
 }
---
+++ api/src/main/java/com/ayo/api/exceptions/NoNetworkException.kt
@@ -0,0 +1,3 @@
+package com.ayo.api.exceptions
+
+class NoNetworkException : Throwable("No internet connection available.")
